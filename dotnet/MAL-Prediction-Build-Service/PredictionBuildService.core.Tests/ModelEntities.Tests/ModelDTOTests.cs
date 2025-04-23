b6ea15e00c0ded0c9bddf1efd24b630cf4e4045a
Kristian <86206683+Dogmeat2000@users.noreply.github.com>
2025-04-23 14:25:18 +0000

    Add: UnitTest for class: ModelDTO.cs in PredictionBuildService.core)

---
+++ dotnet/MAL-Prediction-Build-Service/PredictionBuildService.core.Tests/ModelEntities.Tests/ModelDTOTests.cs
@@ -0,0 +1,10 @@
+namespace PredictionBuildService.core.Tests.ModelEntities.Tests;
+
+public class ModelDTOTests
+{
+    [Fact]
+    public void ToString_ConvertsSuccessfully_ReturnsString() {
+        //TODO: Not implemented yet. Awaiting final python implementation metadata logic.
+        Assert.True(true);
+    }
+}
