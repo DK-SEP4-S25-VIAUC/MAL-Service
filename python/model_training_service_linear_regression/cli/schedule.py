45eb5324059e4120a1ff7cc2895f936aaa72010e
root <katrine.amaand@icloud.com>
2025-05-13 10:34:46 +0000

    updating path

--- python/model_training_service_linear_regression/cli/schedule.py
+++ python/model_training_service_linear_regression/cli/schedule.py
@@ -1,5 +1,5 @@
 # schedule.py
-from model_training_service_linear_regression.model_training_service.scheduler import start_scheduler
+from src.scheduler import start_scheduler
 
 if __name__ == "__main__":
     start_scheduler()
